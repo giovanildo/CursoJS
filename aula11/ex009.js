48fbbebd5a8b0ca9b351d918cff33fc3376d1e31
giovanildo <giovanildos@unilab.edu.br>
2022-10-26 12:53:12 -0300

    Lesson 11 Conditions Part 1

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var naturalidade = 'Brasil'
+var ondeMora = 'França'
+console.log(`Vivendo em ${ondeMora}`)
+if(ondeMora == naturalidade){
+    console.log(`Você nasceu no ${naturalidade}  então é Nativo`)
+} else{
+    console.log(`Você é estrangeiro`)
+}

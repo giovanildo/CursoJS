2d8b5b1673480bad89f627e24cbb277adea0a8e8
giovanildo <giovanildos@unilab.edu.br>
2022-11-10 16:25:56 -0300

    end  javascript lessons setting up firebase hostin

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -3,83 +3,83 @@ function adicionar() {
     let num = document.getElementById('txtNum').value;
     let res = document.getElementById('res');
     let select = document.getElementById('selNum')
-    res.innerHTML = ''
 
 
+    //validação
     if (num.length == 0) {
         alert('O número não pode ser vazio')
         return
     }
-
     num = Number(num);
 
+    if (!isNumero(num)) {
+        alert('O número precisa ficar entre 0 e 100')
+        return
+    }
+
+    if (inLista(num, numeros)) {
+        alert('O número não pode ser repetido')
+        return
+    }
+
+    numeros.push(Number(num))
     let option = document.createElement('option')
     option.text = `Valor ${num} adicionado`
     select.appendChild(option)
 
-    numeros.push(num)
+    res.innerHTML = ''
 
-    let soma = somar(numeros)
-    //validação de dados
-    //adicionar via appendchild
-    //adicionar array
-    //fazer as operações 
-    // mostrar na div res
-    //clicar em adicionar, sempre limpar o  res
-
+    document.getElementById('txtNum').value = ''
+    document.getElementById('txtNum').focus()
 
 }
-
-function somar(array) {
-    let soma = 0;
-
-    for (let index = 0; index < array.length; index++) {
-        soma += array[index];
-    }
-
-    return soma;
-}
-function maior(array) {
-    let maior = 0;
-    for (let index = 0; index < array.length; index++) {
-        let daVez = array[index]
-        if (daVez > maior) {
-            maior = daVez;
-        }
-    }
-    return maior;
-}
-function menor(array) {
-    let menor = 0;
-    for (let index = 0; index < array.length; index++) {
-        const daVez = array[index];
-        if (menor == 0) {
-            menor = daVez
-        }
-
-        if (daVez < menor) {
-            menor = daVez;
-        }
-    }
-    return menor;
-}
-function media(array) {
-    return somar(array)/array.length
-}
-
 
 function finalizar() {
     let res = document.getElementById('res');
-    let texto = `<p>Ao todo, temos ${numeros.length} números cadastrados.</p>`
-    texto += `<p>O maior valor informado foi ${maior(numeros)}</p>`
-    texto += `<p>O menor valor informado foi ${menor(numeros)}</p>`
-    texto += `<p>Somando todos os valores, temos ${somar(numeros)} </p>`
-    texto += `<p>A média dos valores digitados é  ${media(numeros)}</p>`
+
+    if (numeros.length == 0) {
+        alert('Adicione valores')
+        return
+    }
+
+    res.innerHTML = ''
+
+    let total = numeros.length;
+    let maior = numeros[0];
+    let menor = numeros[0];
+    let somar = 0;
+    let media = 0;
+    //teste
+    for (let pos in numeros) {
+        let daVez = numeros[pos];
+        if (daVez > maior) {
+            maior = daVez
+        }
+        if (daVez < menor) {
+            menor = daVez
+        }
+        somar += daVez
+    }
+    media = somar / total
+
+    let texto = `<p>Ao todo, temos ${total} números cadastrados.</p>`
+    texto += `<p>O maior valor informado foi ${maior}</p>`
+    texto += `<p>O menor valor informado foi ${menor}</p>`
+    texto += `<p>Somando todos os valores, temos ${somar} </p>`
+    texto += `<p>A média dos valores digitados é  ${media}</p>`
 
     res.innerHTML = texto
 }
 
-let nums = [1, 10, 5, 7]
-
-
-console.log(somar(nums))
+function isNumero(numero) {
+    if (numero >= 1 && numero <= 100) {
+        return true
+    }
+    return false
+}
+function inLista(numero, lista) {
+    if (lista.indexOf(Number(numero)) != -1) {
+        return true
+    }
+    return false
+}
